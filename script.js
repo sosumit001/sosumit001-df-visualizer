755056a59fc33d3cd5bc4c09298fe289f6d6eec1
Hello Isf <23234234@gmail.com>
2022-06-21 03:28:06 +0530

    -- module updated

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import arrayEle from "./components/array/components.array.js";
+import {Node , bstSvg }from "./components/binary/component.bst.js";
 
-//draw array element on screen
-var ArrIndx = 0;
+
+//header animation
+
+
 function arrEleOnScreen(val)
 {
     var arrEle = new arrayEle(val,"black",0);
@@ -51,10 +54,107 @@ $("#array-random-btn").on('click',()=>{
     }
     for(let i = 0;i<5;i++)
     {
-        
         arrEleOnScreen(rArrayInp[i]);
     }
 })
 
-       
+ //2. there comes bst part
+
+ //insert element
+ const testsvg = new bstSvg(45,2);
+
+//search element
+$("#bst-insert-btn").on('click',()=>{34
+    var inpVal = parseInt($("#bst-input-ele").val());
+  
+    var svg = testsvg.insert(inpVal);
+   
+    //animate svg
+    $("#bstDisplayFrame").append(svg);
+    
+
+    $("svgObject").find("svg").remove('.bst-svg-effect');
+
+    $( ".bst-svg" ).last().animate({
+        opacity: 1,
+        height:45 + "px",
+        width :45 + "px",
+        backgroundColor:"yellow"
+    }, 800,()=> {});
+    if( $("#bstDisplayFrame svg").length>1){
+        setTimeout(() => {
+            var line = testsvg.creatLine("bst-svg-line");
+            $("#bstDisplayFrame").append(line);
+            
+
+        }, 800);
+    }
+   
+  
+})
+
+// ** aboutPage
+$("#about-button").on('click',()=>{
+    $(".nav-ul").css({
+        "--navVar":-20+"%",
+    })
+})
+
+
+//**Home page
+$("#menu-button").on("click",()=>{
+    $(".nav-ul").css({
+        "--navVar":-79+"%",
+    })
+})
+
+
+// ** visualizer page
+$("#visualizer-button").on('click',()=>{
+   
+    $("#algoListPage").css({
+        "top":0+"%",
+        "border-radius":"0",
+    })
+    $("#algo-header").css({
+        "width":80+"%",
+    })
+    $(".nav-ul").css({
+        "--navVar":100+"%",
+    })
+})
+// ** viualizer to home
+$("#BacktoHome").on('click',()=>{
+    $("#algo-header").css({
+        "width":60+"%",
+    })
+    $("#algoListPage").css({
+        "top":105+"%",
+        "border-radius":"50%",
+    })
+    $(".nav-ul").css({
+        "--navVar":-79+"%",
+    })
+})
+
+
+$("#code-editor-button").on("click",()=>{
+    console.log("asdf")
+    $(".nav-ul").css({
+        "--navVar":-40+"%",
+    })
+})
+
+// controls on visualizer page
+$("#data-list-header-ul").hover(()=>{
+   $("#algo-data-list-scroller").css({
+    "opacity":0.9
+   })
+   $("#data-list-header-ul").on('mouseleave',()=>{
+    $("#algo-data-list-scroller").css({
+        "opacity":0.4
+       })
+   })
+})
+//scroller event
 
